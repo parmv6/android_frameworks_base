3b6a790d0ee38a2fe06bbdc11df13aa9c5e60307
DvTonder <david.vantonder@gmail.com>
2012-11-27 18:08:18 -0500

    Framework: Forward port Long press back to kill app
    Original author: DaneshM
    
    Change-Id: I54f4fb861897fe713cffa15422535c5091a0c095
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	core/res/res/values/symbols.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4498,6 +4498,12 @@ public final class Settings {
         public static final String WIMAX_ON = "wimax_on";
 
         /**
+         * Whether to allow killing of the foreground app by long-pressing the Back button
+         * @hide
+         */
+        public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
