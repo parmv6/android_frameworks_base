7a11f9f3347607b4b65614baa80b21723e45e22b
Jesús David <jedga_95@outlook.com>
2013-05-10 15:16:20 -0500

    Remove settings provider entry

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2550,12 +2550,6 @@ public final class Settings {
         public static final String EXPANDED_DESKTOP_STATE = "expanded_desktop_state";
 
         /**
-         * Restart Launcher
-         * @hide
-         */
-        public static final String EXPANDED_DESKTOP_RESTART_LAUNCHER = "expanded_desktop_restart_launcher";
-
-        /**
         * Override and forcefully disable the fullscreen keyboard
         * @hide
         */
