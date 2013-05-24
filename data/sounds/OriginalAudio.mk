ef6f6e6f1b4c34da25f5fd63affd8ffd20d69f3d
zeelog <zeelog@zte>
2013-05-25 00:49:36 +0200

    Bring back CM old sounds
    Change-Id: I1553a7572218dd97e677a4f4ee5206acb579e0ce

--- data/sounds/OriginalAudio.mk
+++ data/sounds/OriginalAudio.mk
@@ -1,28 +1,22 @@
 #
-# Original audio package that shipped on G1
-# 
-# This file is included from core.mk so that all devices will have these sounds
-# 
-# TODO: Clean up for future releases
+# ogg Audio Files
 #
 
 LOCAL_PATH:= frameworks/base/data/sounds
 
 # Alarms
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/alarms/ogg/Cesium.ogg:system/media/audio/alarms/Cesium.ogg \
-	$(LOCAL_PATH)/alarms/ogg/Fermium.ogg:system/media/audio/alarms/Fermium.ogg \
-	$(LOCAL_PATH)/alarms/ogg/Hassium.ogg:system/media/audio/alarms/Hassium.ogg \
-	$(LOCAL_PATH)/alarms/ogg/Neptunium.ogg:system/media/audio/alarms/Neptunium.ogg \
-	$(LOCAL_PATH)/alarms/ogg/Nobelium.ogg:system/media/audio/alarms/Nobelium.ogg \
-	$(LOCAL_PATH)/alarms/ogg/Osmium.ogg:system/media/audio/alarms/Osmium.ogg \
-	$(LOCAL_PATH)/alarms/ogg/Plutonium.ogg:system/media/audio/alarms/Plutonium.ogg \
-	$(LOCAL_PATH)/alarms/ogg/Promethium.ogg:system/media/audio/alarms/Promethium.ogg
+	$(LOCAL_PATH)/alarms/ogg/Alarm_Beep_01.ogg:system/media/audio/alarms/Alarm_Beep_01.ogg \
+	$(LOCAL_PATH)/alarms/ogg/Alarm_Beep_02.ogg:system/media/audio/alarms/Alarm_Beep_02.ogg \
+	$(LOCAL_PATH)/alarms/ogg/Alarm_Beep_03.ogg:system/media/audio/alarms/Alarm_Beep_03.ogg \
+	$(LOCAL_PATH)/alarms/ogg/Alarm_Buzzer.ogg:system/media/audio/alarms/Alarm_Buzzer.ogg \
+	$(LOCAL_PATH)/alarms/ogg/Alarm_Classic.ogg:system/media/audio/alarms/Alarm_Classic.ogg \
+	$(LOCAL_PATH)/alarms/ogg/Alarm_Rooster_01.ogg:system/media/audio/alarms/Alarm_Rooster_01.ogg \
+	$(LOCAL_PATH)/alarms/ogg/Alarm_Rooster_02.ogg:system/media/audio/alarms/Alarm_Rooster_02.ogg \
 
 # Effects
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/effects/ogg/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
-	$(LOCAL_PATH)/effects/ogg/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
 	$(LOCAL_PATH)/effects/ogg/Dock.ogg:system/media/audio/ui/Dock.ogg \
 	$(LOCAL_PATH)/effects/ogg/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
 	$(LOCAL_PATH)/effects/ogg/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
@@ -31,57 +25,49 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/effects/ogg/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
 	$(LOCAL_PATH)/effects/ogg/Lock.ogg:system/media/audio/ui/Lock.ogg \
 	$(LOCAL_PATH)/effects/ogg/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
-	$(LOCAL_PATH)/effects/ogg/Media_Volume.ogg:system/media/audio/ui/Media_Volume.ogg \
+	$(LOCAL_PATH)/effects/ogg/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
 	$(LOCAL_PATH)/effects/ogg/Undock.ogg:system/media/audio/ui/Undock.ogg \
 	$(LOCAL_PATH)/effects/ogg/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
-	$(LOCAL_PATH)/effects/ogg/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
-	$(LOCAL_PATH)/effects/ogg/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
-	$(LOCAL_PATH)/effects/ogg/VolumeIncremental.ogg:system/media/audio/ui/VolumeIncremental.ogg \
-    $(LOCAL_PATH)/effects/ogg/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg
 
 # Notifications
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/notifications/ogg/Adara.ogg:system/media/audio/notifications/Adara.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Arcturus.ogg:system/media/audio/notifications/Arcturus.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Bellatrix.ogg:system/media/audio/notifications/Bellatrix.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Capella.ogg:system/media/audio/notifications/Capella.ogg \
-	$(LOCAL_PATH)/notifications/ogg/CetiAlpha.ogg:system/media/audio/notifications/CetiAlpha.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Hojus.ogg:system/media/audio/notifications/Hojus.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Lalande.ogg:system/media/audio/notifications/Lalande.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Mira.ogg:system/media/audio/notifications/Mira.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Polaris.ogg:system/media/audio/notifications/Polaris.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Pollux.ogg:system/media/audio/notifications/Pollux.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Procyon.ogg:system/media/audio/notifications/Procyon.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Proxima.ogg:system/media/audio/notifications/Proxima.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Shaula.ogg:system/media/audio/notifications/Shaula.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Spica.ogg:system/media/audio/notifications/Spica.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Tejat.ogg:system/media/audio/notifications/Tejat.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Upsilon.ogg:system/media/audio/notifications/Upsilon.ogg \
-	$(LOCAL_PATH)/notifications/ogg/Vega.ogg:system/media/audio/notifications/Vega.ogg
+	$(LOCAL_PATH)/notifications/ogg/Beat_Box_Android.ogg:system/media/audio/notifications/Beat_Box_Android.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Bees_Knees.ogg:system/media/audio/notifications/Bees_Knees.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Cheeper.ogg:system/media/audio/notifications/Cheeper.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Cricket.ogg:system/media/audio/notifications/Cricket.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Doink.ogg:system/media/audio/notifications/Doink.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Drip.ogg:system/media/audio/notifications/Drip.ogg \
+	$(LOCAL_PATH)/notifications/ogg/F1_MissedCall.ogg:system/media/audio/notifications/F1_MissedCall.ogg \
+	$(LOCAL_PATH)/notifications/ogg/F1_New_MMS.ogg:system/media/audio/notifications/F1_New_MMS.ogg \
+	$(LOCAL_PATH)/notifications/ogg/F1_New_SMS.ogg:system/media/audio/notifications/F1_New_SMS.ogg \
+	$(LOCAL_PATH)/notifications/ogg/F1_NewVoicemail.ogg:system/media/audio/notifications/F1_NewVoicemail \
+	$(LOCAL_PATH)/notifications/ogg/Heaven.ogg:system/media/audio/notifications/Heaven.ogg \
+	$(LOCAL_PATH)/notifications/ogg/IM_Me.ogg:system/media/audio/notifications/IM_Me.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Moonbeam.ogg:system/media/audio/notifications/Moonbeam.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Pixiedust.ogg:system/media/audio/notifications/Pixiedust.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Pizzicato.ogg:system/media/audio/notifications/Pizzicato.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Plastic_Pipe.ogg:system/media/audio/notifications/Plastic_Pipe.ogg \
+	$(LOCAL_PATH)/notifications/ogg/ShortCircuit.ogg:system/media/audio/notifications/ShortCircuit.ogg \
+	$(LOCAL_PATH)/notifications/ogg/SpaceSeed.ogg:system/media/audio/notifications/SpaceSeed.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Star_Struck.ogg:system/media/audio/notifications/Star_Struck.ogg \
+	$(LOCAL_PATH)/notifications/ogg/TaDa.ogg:system/media/audio/notifications/TaDa.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Tinkerbell.ogg:system/media/audio/notifications/Tinkerbell.ogg \
+	$(LOCAL_PATH)/notifications/ogg/Tweeters.ogg:system/media/audio/notifications/Tweeters.ogg \
 
 # Ringtones
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/ringtones/ogg/Andromeda.ogg:system/media/audio/ringtones/Andromeda.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Aquila.ogg:system/media/audio/ringtones/Aquila.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/ArgoNavis.ogg:system/media/audio/ringtones/ArgoNavis.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/CanisMajor.ogg:system/media/audio/ringtones/CanisMajor.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Carina.ogg:system/media/audio/ringtones/Carina.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Centaurus.ogg:system/media/audio/ringtones/Centaurus.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Cygnus.ogg:system/media/audio/ringtones/Cygnus.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Draco.ogg:system/media/audio/ringtones/Draco.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Girtab.ogg:system/media/audio/ringtones/Girtab.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Hydra.ogg:system/media/audio/ringtones/Hydra.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Machina.ogg:system/media/audio/ringtones/Machina.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Orion.ogg:system/media/audio/ringtones/Orion.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Pegasus.ogg:system/media/audio/ringtones/Pegasus.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Perseus.ogg:system/media/audio/ringtones/Perseus.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Pyxis.ogg:system/media/audio/ringtones/Pyxis.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/RadiationOrchestration.ogg:system/media/audio/ringtones/RadiationOrchestration.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Rigel.ogg:system/media/audio/ringtones/Rigel.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/RobotsforEveryone.ogg:system/media/audio/ringtones/RobotsforEveryone.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Scarabaeus.ogg:system/media/audio/ringtones/Scarabaeus.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Sceptrum.ogg:system/media/audio/ringtones/Sceptrum.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Solarium.ogg:system/media/audio/ringtones/Solarium.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Themos.ogg:system/media/audio/ringtones/Themos.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/UrsaMinor.ogg:system/media/audio/ringtones/UrsaMinor.ogg \
-	$(LOCAL_PATH)/ringtones/ogg/Zeta.ogg:system/media/audio/ringtones/Zeta.ogg
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Classic_01.ogg:system/media/audio/ringtones/Ring_Classic_01.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Classic_02.ogg:system/media/audio/ringtones/Ring_Classic_02.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Classic_03.ogg:system/media/audio/ringtones/Ring_Classic_03.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Classic_04.ogg:system/media/audio/ringtones/Ring_Classic_04.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Classic_05.ogg:system/media/audio/ringtones/Ring_Classic_05.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Digital_01.ogg:system/media/audio/ringtones/Ring_Digital_01.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Digital_02.ogg:system/media/audio/ringtones/Ring_Digital_02.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Digital_03.ogg:system/media/audio/ringtones/Ring_Digital_03.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Digital_04.ogg:system/media/audio/ringtones/Ring_Digital_04.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Digital_05.ogg:system/media/audio/ringtones/Ring_Digital_05.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Synth_01.ogg:system/media/audio/ringtones/Ring_Synth_01.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Synth_02.ogg:system/media/audio/ringtones/Ring_Synth_02.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Synth_03.ogg:system/media/audio/ringtones/Ring_Synth_03.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Synth_04.ogg:system/media/audio/ringtones/Ring_Synth_04.ogg \
+	$(LOCAL_PATH)/ringtones/ogg/Ring_Synth_05.ogg:system/media/audio/ringtones/Ring_Synth_05.ogg \
