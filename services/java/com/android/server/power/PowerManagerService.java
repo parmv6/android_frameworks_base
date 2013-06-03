a8766e6678231e4c47bfea58c103ff4c4bc04bf9
maniac103 <dannybaumann@web.de>
2013-06-03 13:07:57 +0200

    Improve auto-brightness handling - Add a setting to control the auto-brightness algorithm responsitivity - Make the algorithm react much faster when the ambient brightness   increases quickly, e.g. when receiving a call in bright sunlight.
    Conflicts:
    	services/java/com/android/server/power/PowerManagerService.java
    
    Change-Id: If9a6790c95ce8c2afdb40b21ecd5cafd443ce5a3

--- services/java/com/android/server/power/PowerManagerService.java
+++ services/java/com/android/server/power/PowerManagerService.java
@@ -331,6 +331,9 @@ public final class PowerManagerService extends IPowerManager.Stub
     // Use 0 if there is no adjustment.
     private float mScreenAutoBrightnessAdjustmentSetting;
 
+    // The screen auto-brightness responsitivity factor, from 0.2 to 3.
+    private float mAutoBrightnessResponsitivityFactor;
+
     // The screen brightness mode.
     // One of the Settings.System.SCREEN_BRIGHTNESS_MODE_* constants.
     private int mScreenBrightnessModeSetting;
@@ -498,6 +501,8 @@ public final class PowerManagerService extends IPowerManager.Stub
                     false, mSettingsObserver, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BUTTON_BACKLIGHT_DISABLE),
+	    resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.AUTO_BRIGHTNESS_RESPONSIVENESS),
                     false, mSettingsObserver, UserHandle.USER_ALL);
 
             // Go.
@@ -570,6 +575,11 @@ public final class PowerManagerService extends IPowerManager.Stub
         if (mButtonsLightDisabled)
             mButtonsLight.setBrightness(0);
 
+        final float newAutoBrightnessResponsitivityFactor = Settings.System.getFloatForUser(resolver,
+                Settings.System.AUTO_BRIGHTNESS_RESPONSIVENESS, 1.0f,
+                UserHandle.USER_CURRENT);
+        mAutoBrightnessResponsitivityFactor =
+                Math.min(Math.max(newAutoBrightnessResponsitivityFactor, 0.2f), 3.0f);
         mDirty |= DIRTY_SETTINGS;
     }
 
@@ -1660,6 +1670,8 @@ public final class PowerManagerService extends IPowerManager.Stub
             mDisplayPowerRequest.useProximitySensor = shouldUseProximitySensorLocked();
 
             mDisplayPowerRequest.blockScreenOn = mScreenOnBlocker.isHeld();
+
+            mDisplayPowerRequest.responsitivityFactor = mAutoBrightnessResponsitivityFactor;
 
             mDisplayReady = mDisplayPowerController.requestPowerState(mDisplayPowerRequest,
                     mRequestWaitForNegativeProximity);
